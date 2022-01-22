19f32717cffe5145350f46c2f32aec8fd8e8c278
Thomas Vincent <vrince@gmail.com>
2022-01-22 13:16:21 -0500

    remove old v1 files

--- run-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export FLASK_APP=$(pwd)/service.py
-export FLASK_ENV=development
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-pkill -f "flask run --host=0.0.0.0 --port=5000"
-flask run --host=0.0.0.0 --port=5000
